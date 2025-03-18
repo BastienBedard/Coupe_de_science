5321e4825dde519b663fbd8e73d7c5bb2f650930
Michiboi29 <emichaud29@gmail.com>
2025-03-18 08:43:04 -0400

    FORMATAGE ZONE DE JEU

--- zone_de_jeu_backup.py
+++ zone_de_jeu_backup.py
@@ -6,7 +6,12 @@ puis écriver les différentes commandes que vous voulez donner au vaisseau.
 """
 from main import play
 
+
 EQUIPE = 0
 NIVEAU = 0
 coups = [[2, 'avant'], 'tdroite', [4, [2, 'avant'], 'tgauche']]
+
+
+
+
 play(equipe = EQUIPE, niveau = NIVEAU, moves = coups)
